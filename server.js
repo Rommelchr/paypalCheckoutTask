98eb1845904c96a62ca1ef23141f40c2b5118c7c
Rommelchr <g7_chin18@ens.cnyn.unam.mx>
2024-07-22 17:26:37 -0700

    Fix app variable definition and server setup

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ require('dotenv-safe').config({
     allowEmptyValues: true
   });
   
+const app = require('./src/app');
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
